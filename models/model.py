e47d35d1885dc781739eb50fc25c9edfc7310094
sabadijou <s.abadijou@gmail.com>
2023-01-07 18:34:25 +0330

    smp is used to implement U-Net model

---
+++ models/model.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import segmentation_models_pytorch as smp
+
+
+class Model(nn.Module):
+    def __init__(self, encoder_network='resnet34', encoder_depth=5, input_ch=3, out_channels=1):
+        super(Model, self).__init__()
+        self.unet = smp.Unet(encoder_name=encoder_network,
+                             encoder_depth=encoder_depth,
+                             encoder_weights='imagenet',
+                             in_channels=input_ch,
+                             classes=out_channels)
+
+    def forward(self, x):
+        output = self.unet(x)
+        return output
