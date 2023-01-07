27a42a48422085ba34f265f52a6114056ba36edb
sabadijou <s.abadijou@gmail.com>
2023-01-07 19:06:06 +0330

    modified for current implementation

---
+++ dataset/dataset.py
@@ -0,0 +1,76 @@
+'''
+Source: https://colab.research.google.com/drive/1vYZYXDMfs9hK6KvXY3v1iTPiln9OeJZE?usp=sharing#scrollTo=R6nhF1um1VBZ
+
+Modified by https://github.com/sabadijou
+'''
+
+import torchvision.transforms as transforms
+from scipy import ndimage
+import torch.utils.data
+from glob import glob
+import numpy as np
+import torch
+import cv2
+
+
+class ArealDataset(torch.utils.data.Dataset):
+    def __init__(self, root, training):
+        super(ArealDataset, self).__init__()
+        self.root = root
+        self.training = training
+        self.transform = transforms.Compose([transforms.ColorJitter(.1, .1, .1, .1),
+                                             transforms.GaussianBlur(3, sigma=(0.1, 2.0))])
+        self.IMG_NAMES = sorted(glob(self.root + '/*/images/*.jpg'))
+
+        self.BGR_classes = {'Water': [41, 169, 226],
+                            'Land': [246, 41, 132],
+                            'Road': [228, 193, 110],
+                            'Building': [152, 16, 60],
+                            'Vegetation': [58, 221, 254],
+                            'Unlabeled': [155, 155, 155]}
+
+        self.bin_classes = ['Water', 'Land', 'Road', 'Building', 'Vegetation', 'Unlabeled']
+
+    def __getitem__(self, idx):
+        print(self.IMG_NAMES)
+        img_path = self.IMG_NAMES[idx]
+        mask_path = img_path.replace('images', 'masks').replace('.jpg', '.png')
+        image = cv2.imread(img_path)
+        mask = cv2.imread(mask_path)
+        cls_mask = np.zeros(mask.shape)
+        cls_mask[mask == self.BGR_classes['Water']] = self.bin_classes.index('Water')
+        cls_mask[mask == self.BGR_classes['Land']] = self.bin_classes.index('Land')
+        cls_mask[mask == self.BGR_classes['Road']] = self.bin_classes.index('Road')
+        cls_mask[mask == self.BGR_classes['Building']] = self.bin_classes.index('Building')
+        cls_mask[mask == self.BGR_classes['Vegetation']] = self.bin_classes.index('Vegetation')
+        cls_mask[mask == self.BGR_classes['Unlabeled']] = self.bin_classes.index('Unlabeled')
+        cls_mask = cls_mask[:, :, 0]
+
+        if self.training:
+            if self.transform:
+                image = transforms.functional.to_pil_image(image)
+                image = self.transform(image)
+                image = np.array(image)
+
+            # 90 degree rotation
+            if np.random.rand() < 0.5:
+                angle = np.random.randint(4) * 90
+                image = ndimage.rotate(image, angle, reshape=True)
+                cls_mask = ndimage.rotate(cls_mask, angle, reshape=True)
+
+            if np.random.rand() < 0.5:
+                image = np.flip(image, 0)
+                cls_mask = np.flip(cls_mask, 0)
+
+            if np.random.rand() < 0.5:
+                image = np.flip(image, 1)
+                cls_mask = np.flip(cls_mask, 1)
+
+        image = cv2.resize(image, (512, 512)) / 255.0
+        cls_mask = cv2.resize(cls_mask, (512, 512))
+        image = np.moveaxis(image, -1, 0)
+
+        return torch.tensor(image).float(), torch.tensor(cls_mask, dtype=torch.int64)
+
+    def __len__(self):
+        return len(self.IMG_NAMES)
---
+++ utils/losses.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class JaccardLoss(nn.Module):
+    def __init__(self, n_classes=2):
+        super(JaccardLoss, self).__init__ ()
+        self.classes = n_classes
+
+    def to_one_hot(self, tensor):
+        n, h, w = tensor.size()
+        one_hot = torch.zeros(n, self.classes, h, w).to(tensor.device).scatter_(1, tensor.view(n, 1, h, w), 1)
+        return one_hot
+
+    def forward(self, inputs, target):
+        N = inputs.size ()[0]
+        inputs = F.softmax(inputs, dim=1)
+        target_oneHot = self.to_one_hot(target)
+        inter = inputs * target_oneHot
+        inter = inter.view(N, self.classes, -1).sum(2)
+        union = inputs + target_oneHot - (inputs * target_oneHot)
+        union = union.view(N, self.classes, -1).sum(2)
+        loss = inter / union
+        return 1 - loss.mean()
+
+
+class ArealLoss(nn.Module):
+    def __init__(self, num_classes=6):
+        super(ArealLoss, self).__init__()
+        weights = torch.ones(num_classes)
+        weights = weights.cuda()
+        self.criterion_1 = torch.nn.NLLLoss(weight=weights, reduction='mean').cuda()
+        self.criterion_2 = JaccardLoss(n_classes=num_classes)
+
+    def forward(self, x, y):
+        return self.criterion_1(x, y) + self.criterion_2(x, y)
+
