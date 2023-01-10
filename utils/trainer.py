5592314227dda3453a52ae3a2a04cffbfb56e270
sabadijou <s.abadijou@gmail.com>
2023-01-10 17:03:38 +0330

    init class

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,13 +1,21 @@
 from dataset.dataset import ArealDataset
+from dataset_builder import Builder
 
 
 class Trainer:
-    def __init__(self, model, ds_root, world_size=1, epochs=100, batch_size=4):
+    def __init__(self, model, ds_root, world_size=1, epochs=100,
+                 batch_size=4, train_test_split=0.1, workers=1):
         super(Trainer, self).__init__()
-        self.dataset = ds_root
+        self.dataset = Builder(dataset_root=ds_root,
+                               batch_size=batch_size,
+                               train_test_split_p=train_test_split,
+                               workers=workers)
+
         self.world_size = world_size
         self.epochs = epochs
         self.model = model
+        self.train_dataloader = self.dataset.train_dataloader
+        self.test_dataloader = self.dataset.test_dataloader
 
     def run_train(self):
         self.model.train()
@@ -15,5 +23,5 @@ class Trainer:
             loss_epoch = self.train_step()
 
     def train_step(self):
-        for batch_idx, (x, y) in enumerate(self.train_loader):
+        for batch_idx, (x, y) in enumerate(self.train_dataloader):
             pass
