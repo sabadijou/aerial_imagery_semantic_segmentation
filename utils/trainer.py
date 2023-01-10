1e7b4991dc08d8e6bd4a4fc4d546b03b012b9be7
sabadijou <s.abadijou@gmail.com>
2023-01-10 18:29:00 +0330

    loss and optim are added

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,10 +1,12 @@
 from dataset.dataset import ArealDataset
 from dataset_builder import Builder
-
+from losses import ArealLoss
+from optimizer import ArealOptim
 
 class Trainer:
-    def __init__(self, model, ds_root, world_size=1, epochs=100,
-                 batch_size=4, train_test_split=0.1, workers=1):
+    def __init__(self, cfg, model, ds_root, world_size=1, epochs=100,
+                 batch_size=4, train_test_split=0.1, workers=1,
+                 num_classes=6):
         super(Trainer, self).__init__()
         self.dataset = Builder(dataset_root=ds_root,
                                batch_size=batch_size,
@@ -16,6 +18,9 @@ class Trainer:
         self.model = model
         self.train_dataloader = self.dataset.train_dataloader
         self.test_dataloader = self.dataset.test_dataloader
+        self.criterion = ArealLoss(num_classes=num_classes)
+        self.optimizer_builder = ArealOptim(cfg, model.parameters())
+        self.optimizer = self.optimizer_builder.net_optimizer
 
     def run_train(self):
         self.model.train()
@@ -23,5 +28,13 @@ class Trainer:
             loss_epoch = self.train_step()
 
     def train_step(self):
+        batch_loss_list = []
+        auc_list = []
         for batch_idx, (x, y) in enumerate(self.train_dataloader):
-            pass
+            y_pred = self.model(x)
+            loss = self.criterion(y, y_pred)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            batch_loss_list.append(loss.cpu().detach().numpy())
+            auc_list.append(acc(y, pred_mask).numpy())
