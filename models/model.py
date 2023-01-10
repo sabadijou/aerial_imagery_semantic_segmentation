18d4fee16befc513ac2cce396bc3b84f907f1771
sabadijou <s.abadijou@gmail.com>
2023-01-10 20:49:18 +0330

    pretrained added

--- models/model.py
+++ models/model.py
@@ -3,11 +3,16 @@ import segmentation_models_pytorch as smp
 
 
 class Model(nn.Module):
-    def __init__(self, encoder_network='resnet34', encoder_depth=5, input_ch=3, out_channels=1):
+    def __init__(self, encoder_network='resnet34',
+                 encoder_depth=5,
+                 input_ch=3,
+                 out_channels=1,
+                 pretrained='imagenet'):
+
         super(Model, self).__init__()
         self.unet = smp.Unet(encoder_name=encoder_network,
                              encoder_depth=encoder_depth,
-                             encoder_weights='imagenet',
+                             encoder_weights=pretrained,
                              in_channels=input_ch,
                              classes=out_channels)
 
