7e3691cfea99b13af388de2ebcb6621f074279e3
sabadijou <s.abadijou@gmail.com>
2023-01-10 16:53:54 +0330

    properties are added.ready to unitest

--- utils/dataset_builder.py
+++ utils/dataset_builder.py
@@ -1,23 +1,31 @@
 import torch
 from dataset.dataset import ArealDataset
-train_dataloader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=BACH_SIZE, shuffle=True, num_workers=4)
 
-test_dataloader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=BACH_SIZE, shuffle=False, num_workers=1)
-
-train_dataset, test_dataset =
 
 class Builder:
-    def __init__(self, dataset_root, batch_size=4, train_test_split_p=0.1):
+    def __init__(self, dataset_root, batch_size=4, train_test_split_p=0.1, workers=1):
         super(Builder, self).__init__()
-        self.dataset  = ArealDataset(root=dataset_root, training=True)
-        self.train_dataset, self.test_dataset =  torch.utils.data.random_split(self.dataset,
+        self.dataset = ArealDataset(root=dataset_root, training=True)
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.dataset,
                                                                                [len(self.dataset)-int(train_test_split_p *
                                                                                                       len(self.dataset)),
                                                                                 int(train_test_split_p * len(self.dataset))],
                                                                                generator=torch.Generator().manual_seed(101))
-        self.train_dataloader = torch.utils.data.DataLoader(train_dataset,
+        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset,
                                                             batch_size=batch_size,
                                                             shuffle=True,
-                                                            num_workers=4)
+                                                            num_workers=workers)
+
+        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset,
+                                                           batch_size=batch_size,
+                                                           shuffle=False,
+                                                           num_workers=workers)
+
+    def get_test_dataloader(self):
+        return self.test_dataloader
+
+    def get_train_dataloader(self):
+        return self.train_dataloader
+
+    train_loader = property(get_train_dataloader)
+    test_loader = property(get_test_dataloader)
